9f72e14431a8a5ad4bea2db6b5feb255ec7df5e0
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2024-03-27 14:24:34 -0600

    Updated neovim config, added conform.nvim

---
+++ after/plugin/conform.lua
@@ -0,0 +1,27 @@
+require("conform").setup({
+    formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- Use a sub-list to run only the first available formatter
+        javascript = { { "prettierd", "prettier" } },
+        c = { "clang-format" } -- Assuming you have clang-format installed
+    },
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
+})
+
+require("conform").setup({
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+})
+
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
