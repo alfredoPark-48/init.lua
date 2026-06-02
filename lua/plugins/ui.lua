917cd3647cf1a8add17d063dc71a0df25ec6d593
alfredoPark-48 <parkalfredojeonghyun@gmail.com>
2026-06-02 00:08:41 -0600

    Feat: Updated theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,21 +1,48 @@
 return {
   -- Modern, highly-customizable colorscheme
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- Forces this plugin to load first during startup
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      variant = "default", -- use "light" for light variant, or "auto"
+      transparent = true,  -- Highly recommended! Blends perfectly with your terminal background
+      saturation = 1,      -- Full color punchiness
+      italic_comments = true,
+      hide_fillchars = true,  -- Gives you an incredibly clean looking ui layout
+      borderless_pickers = true, -- Removes ugly borders on Telescope pickers
+      terminal_colors = true,
+      cache = true, -- Speeds up your Neovim boot time significantly
+
+      -- Custom syntax highlight group adjustments
+      highlights = {
+        Comment = { fg = "#696969", bg = "NONE", italic = true },
+      },
+
+      -- Custom extension tracking configurations
+      extensions = {
+        telescope = true,
+        cmp = true,
+        gitsigns = true,
+        notify = true,
+      },
+    },
+    config = function(_, opts)
+      -- Setup cyberdream with the options object table we built right above
+      require("cyberdream").setup(opts)
+      
+      -- Load the color architecture engine 
+      vim.o.background = "dark"
+      vim.cmd("colorscheme cyberdream")
     end,
   },
-
   -- Sleek statusline at the bottom of the screen
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
-        theme = "catppuccin",
+        theme = "cyberdream",
         component_separators = "|",
         section_separators = "",
       },
